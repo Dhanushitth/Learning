61a2fbc6905e075ee7bb39ca1d14351550c5139b
Dhanushitth <parthasarthy691@gmail.com>
2024-06-12 13:53:59 +0530

    test

--- Navigation/HomeScreenNavigation.js
+++ Navigation/HomeScreenNavigation.js
@@ -1,19 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createStackNavigator, } from '@react-navigation/stack';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../Screen/HomeScreen';
-import CourseDetailScreen from '../Screen/CourseDetailScreen'
-import { NavigationContainer } from '@react-navigation/native';
+import CourseDetailScreen from '../Screen/CourseDetailScreen';
 
-const Stack=createStackNavigator();
-export default function HomeScreenNavigation() {
+const Stack = createStackNavigator();
+
+const  HomeScreenNavigation =()=> {
   return (
-    <NavigationContainer independent={true}>
- <Stack.Navigator screenOptions={{headerShown:false}}>
-    <Stack.Screen name='Home'  component={HomeScreen}/>
-    <Stack.Screen name='course-detail'  component={CourseDetailScreen}/>
-
- </Stack.Navigator>
- </NavigationContainer>
-  )
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='Home' component={HomeScreen} />
+      <Stack.Screen name='CourseDetailScreen' component={CourseDetailScreen} />
+    </Stack.Navigator>
+  );
 }
+export default HomeScreenNavigation;
